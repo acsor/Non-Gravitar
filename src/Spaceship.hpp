8f3765e306c347069fa01929c969d43224e0aea8
Acsor <nildexo@yandex.com>
2019-02-27 20:57:30 +0100

    Improve Spaceship texture and rename Fuel.* texture files to fuel.*

--- src/Spaceship.hpp
+++ src/Spaceship.hpp
@@ -44,8 +44,8 @@ namespace gvt {
 			 * updated accordingly (gruntwork, but avoids the use of an
 			 * external image library).
 			 */
-			static unsigned const constexpr WIDTH = 42;
-			static unsigned const constexpr HEIGHT = 49;
+			static unsigned const constexpr WIDTH = 46;
+			static unsigned const constexpr HEIGHT = 42;
 
 			unsigned fuel() const;
 			/**
