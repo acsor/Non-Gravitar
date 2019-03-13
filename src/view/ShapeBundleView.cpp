0c9d14c9adbfbf8f788d6c069f39a0d6de53d9b2
Acsor <nildexo@yandex.com>
2019-03-13 21:20:05 +0100

    Define the ShapeBundleView class

---
+++ src/view/ShapeBundleView.cpp
@@ -0,0 +1,89 @@
+// MIT License
+// 
+// Copyright (c) 2018 Oscar B. et al.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#include "ShapeBundleView.hpp"
+
+#include <stdexcept>
+#include "../view/SpaceshipView.hpp"
+#include "../view/BunkerView.hpp"
+
+using ShapeBundleView = gvt::ShapeBundleView;
+
+
+ShapeBundleView::ShapeBundleView(shared_ptr<ShapeBundle> bundle, bool debug):
+	mBundle{bundle}, mDebug{debug} {
+	bundle->attachListener(*this);
+}
+
+ShapeBundleView::~ShapeBundleView() {
+	if (auto p = mBundle.lock())
+		p->detachListener(*this);
+}
+
+gvt::ShapeView* ShapeBundleView::shapeToView (shared_ptr<Shape> shape) {
+	if (std::dynamic_pointer_cast<Spaceship>(shape)) {
+		return new SpaceshipView(
+			std::dynamic_pointer_cast<Spaceship>(shape), mDebug
+		);
+	} else if (std::dynamic_pointer_cast<Bunker>(shape)) {
+		return new BunkerView(
+			std::dynamic_pointer_cast<Bunker>(shape), mDebug
+		);
+	} else {
+		throw std::domain_error("Unrecognized type of shape");
+	}
+}
+
+
+bool ShapeBundleView::debug() const {
+	return mDebug;
+}
+
+void ShapeBundleView::debug(bool state) {
+	for (auto i = mViews.begin(); i != mViews.end(); i++) {
+		i->second->debug(state);
+	}
+}
+
+void ShapeBundleView::draw(
+	sf::RenderTarget &target, sf::RenderStates state
+) const {
+	for (auto i = mViews.begin(); i != mViews.end(); i++) {
+		i->second->draw(target, state);
+	}
+}
+
+void ShapeBundleView::handle(Event e) {
+	shared_ptr<Shape> shape;
+
+	if (e == ShapeBundle::SHAPE_ATTACHED) {
+		shape = *static_cast<shared_ptr<Shape>*>(e.data);
+
+		mViews[shape.get()] = unique_ptr<ShapeView>(shapeToView(shape));
+	} else if (e == ShapeBundle::SHAPE_DETACHED) {
+		shape = *static_cast<shared_ptr<Shape>*>(e.data);
+
+		mViews.erase(shape.get());
+	} else if (e == ShapeBundle::DESTROIED) {
+		mViews.clear();
+		mBundle.reset();
+	}
+}
---
+++ src/view/ShapeBundleView.hpp
@@ -0,0 +1,63 @@
+// MIT License
+// 
+// Copyright (c) 2018 Oscar B. et al.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#ifndef NON_GRAVITAR_SHAPE_BUNDLE_VIEW
+#define NON_GRAVITAR_SHAPE_BUNDLE_VIEW
+
+#include <map>
+#include <memory>
+#include <SFML/Graphics.hpp>
+#include "../Event.hpp"
+#include "../model/ShapeBundle.hpp"
+#include "../view/ShapeView.hpp"
+
+template<typename T> using weak_ptr = std::weak_ptr<T>;
+template<typename T> using shared_ptr = std::shared_ptr<T>;
+template<typename T> using unique_ptr = std::unique_ptr<T>;
+
+
+namespace gvt {
+	class ShapeBundleView: public sf::Drawable, public EventListener {
+		protected:
+			weak_ptr<ShapeBundle> mBundle;
+			std::map<Shape*, unique_ptr<ShapeView>> mViews;
+
+			bool mDebug;
+
+			ShapeView* shapeToView (shared_ptr<Shape> shape);
+		public:
+			ShapeBundleView(
+				shared_ptr<ShapeBundle> bundle, bool debug=false
+			);
+			~ShapeBundleView();
+
+			bool debug() const;
+			void debug(bool state);
+
+			void draw(
+				sf::RenderTarget &target, sf::RenderStates state
+			) const override;
+			void handle(Event e) override;
+	};
+}
+
+
+#endif
