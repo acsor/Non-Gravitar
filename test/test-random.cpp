88b46baf8c9c4105a0303319658764cde6db6ba8
Acsor <nildexo@yandex.com>
2019-10-08 21:58:28 +0200

    Add simple test for gvt::UniRandom

---
+++ test/test-random.cpp
@@ -0,0 +1,45 @@
+// MIT License
+//
+// Copyright (c) 2018 Oscar B. et al.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#include <set>
+#include "catch.hpp"
+#include "utils/Random.hpp"
+
+
+TEST_CASE(
+		"UniRandom must produce all values in the given range", "[UniRandom]"
+) {
+	unsigned const bottom = 0, top = 100;
+	gvt::UniRandom<unsigned> r {bottom, top};
+	std::set<unsigned> actual, expected;
+	unsigned trials = 1E3;
+
+	for (auto i = bottom; i <= top; i++)
+		expected.insert(i);
+
+	while (trials > 0) {
+		actual.insert(r());
+
+		trials--;
+	}
+
+	REQUIRE(actual == expected);
+}
